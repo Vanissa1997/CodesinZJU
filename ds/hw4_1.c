f526f74dbb86195f95c1ae6d03052354891fda50
xps13 <1075339263@qq.com>
2017-10-20 18:10:02 +0800

    bingo

--- ds/hw4_1.c
+++ ds/hw4_1.c
@@ -21,43 +21,67 @@ void Level_order ( Tree T, void (*visit)(Tree ThisNode) );
 
 int main()
 {
-    Tree T;
-    T->Element=3;
-    T->Left->Element=5;
-    T->Left->Left->Element=1;
-    T->Left->Right->Element=8;
-    T->Left->Right->Left->Element=9;
-    T->Right->Element=6;
-    T->Right->Right->Element=10;
-    //Tree T = BuildTree();
+    Tree T = BuildTree();
     printf("Level-order:");
     Level_order(T, PrintNode);
     return 0;
 }
 
+
+
 void Level_order ( Tree T, void (*visit)(Tree ThisNode) )
 {
     int Maxsize=1000;
     Tree a[Maxsize];
-    int Front=0,Rear=0;
+    int Front=0,Rear=0,i=0;
     Tree p=T;
-    a[Rear]=p;
+    if(T==NULL)
+    {
+        return;
+    }
+   // a[Front]=(Tree)malloc(sizeof(struct TreeNode)); 
+    a[Front]=p;
 
     while(1)
     {       
-        if(a[Front]->Left!=NULL)
+        if(a[i]->Left)
         {
-             Rear=(Rear+1)/Maxsize;
-             a[Rear]=a[Rear-1]->Left;         
+             //Rear=(Rear+1)%Maxsize;
+             Rear++;
+             a[Rear]=a[i]->Left;         
         }
-        if(a[Front]->Right!=NULL)
+        if(a[i]->Right)
         {
-            Rear=(Rear+1)/Maxsize;
-            a[Rear]=a[Rear-1]->Right;  
+            //Rear=(Rear+1)%Maxsize;
+            Rear++;
+            a[Rear]=a[i]->Right;  
         }
-        (*visit)(a[Front]);
-        Front=(Front+1)/Maxsize;
-        if(Front==Rear)
-        break;
+        //Front=(Front+1)/Maxsize;
+        i++;
+        if(i>Rear)
+            break;
     } 
+    for(i=0;i<=Rear;i++)
+        (*visit)(a[i]);
+    return;
+}       
+    
+Tree BuildTree()
+{
+    Tree T;
+    T=(Tree)malloc(sizeof(struct TreeNode)); 
+    T->Element=3;
+    T->Left=(Tree)malloc(sizeof(struct TreeNode));
+    T->Left->Element=5;
+    T->Left->Left=(Tree)malloc(sizeof(struct TreeNode));
+    T->Left->Left->Element=1;
+    T->Left->Right=(Tree)malloc(sizeof(struct TreeNode));
+    T->Left->Right->Element=8;
+    T->Left->Right->Left=(Tree)malloc(sizeof(struct TreeNode));
+    T->Left->Right->Left->Element=9;
+    T->Right=(Tree)malloc(sizeof(struct TreeNode));
+    T->Right->Element=6;
+    T->Right->Right=(Tree)malloc(sizeof(struct TreeNode));
+    T->Right->Right->Element=10;
+    return T;
 }
